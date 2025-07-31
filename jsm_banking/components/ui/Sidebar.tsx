7fb5b7bcf71dbabeb91ce42ee87271c878662aee
MenuMinyu <130168857+MenuMinyu@users.noreply.github.com>
2025-07-31 15:20:44 +0000

    Update Sidebar.tsx

--- jsm_banking/components/ui/Sidebar.tsx
+++ jsm_banking/components/ui/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
             </Link>
 
             {sidebarLinks.map((item) => {
-                const isActive = pathname === item.route || pathname.startsWith('${item.route}/')
+                const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
                 return (
                     <Link href={item.route} key={item.label} className={cn('sidebar-link', 'bg-bank-gradient', isActive )}>
                         <div className="relative size-6">
